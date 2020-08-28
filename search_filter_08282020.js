5b59dc0f2d256d4d334a64367bbaac158de7cdd4
soonyi-sxy530 <55300935+soonyi-sxy530@users.noreply.github.com>
2020-08-28 16:54:32 -0400

    search and filter function

---
+++ search_filter_08282020.js
@@ -0,0 +1,282 @@
+//
+//  BEWARE!!
+//  This is exteremely raw and brute-force approach.
+//  0 elegancy in coding whatsoever, but it does the job.
+//  one could make function that looks at 32 binary-coded state....
+//  but I'm just gonna do it this way because this will work and probably take less time for debugging.
+//
+//
+$(document).ready(function () {
+    $("#speciesFilter, #groupFilter, #proteinFilter, #localizationFilter, #bprocessFilter").on("change", function () {
+        var species = $('#speciesFilter').find("option:selected").val();
+        var group = $('#groupFilter').val();
+        var protein = $('#proteinFilter').val();
+        var localization = $('#localizationFilter').val();
+        var bprocess = $('#bprocessFilter').val();
+        SearchData(species, group, protein, localization, bprocess)
+    });
+});
+function SearchData(species, group, protein, localization, bprocess) {
+    // 00000
+    if (species == 'all' && group == '' && protein == '' && localization == '' && bprocess == '') {
+        $('#helicasedb tbody tr').show();
+    }
+    else {
+        $('#helicasedb tbody tr:has(td)').each(function () {
+            var rowSpecies = $.trim($(this).find('td:nth-child(1)').text());
+            var rowGroup = $.trim($(this).find('td:nth-child(2)').text());
+            var rowProtein = $.trim($(this).find('td:nth-child(3)').text());
+            var rowLocalization = $.trim($(this).find('td:nth-child(4)').text());
+            var rowBProcess = $.trim($(this).find('td:nth-child(5)').text());
+
+            // 10000
+            if (species != 'all' && group.toUpperCase() == '' && protein.toUpperCase() == '' && localization.toUpperCase() == '' && bprocess.toUpperCase() == '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 01000
+            else if (species == 'all' && group.toUpperCase() != '' && protein.toUpperCase() == '' && localization.toUpperCase() == '' && bprocess.toUpperCase() == '') {
+              if (rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 00100
+            else if (species == 'all' && group.toUpperCase() == '' && protein.toUpperCase() != '' && localization.toUpperCase() == '' && bprocess.toUpperCase() == '') {
+              if (rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 00010
+            else if (species == 'all' && group.toUpperCase() == '' && protein.toUpperCase() == '' && localization.toUpperCase() != '' && bprocess.toUpperCase() == '') {
+              if (rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 00001
+            else if (species == 'all' && group.toUpperCase() == '' && protein.toUpperCase() == '' && localization.toUpperCase() == '' && bprocess.toUpperCase() != '') {
+              if (rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 10001
+            else if (species != 'all' && group.toUpperCase() == '' && protein.toUpperCase() == '' && localization.toUpperCase() == '' && bprocess.toUpperCase() != '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 10010
+            else if (species != 'all' && group.toUpperCase() == '' && protein.toUpperCase() == '' && localization.toUpperCase() != '' && bprocess.toUpperCase() == '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 10011
+            else if (species != 'all' && group.toUpperCase() == '' && protein.toUpperCase() == '' && localization.toUpperCase() != '' && bprocess.toUpperCase() != '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 10100
+            else if (species != 'all' && group.toUpperCase() == '' && protein.toUpperCase() != '' && localization.toUpperCase() == '' && bprocess.toUpperCase() == '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 10101
+            else if (species != 'all' && group.toUpperCase() == '' && protein.toUpperCase() != '' && localization.toUpperCase() == '' && bprocess.toUpperCase() != '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 10110
+            else if (species != 'all' && group.toUpperCase() == '' && protein.toUpperCase() != '' && localization.toUpperCase() != '' && bprocess.toUpperCase() == '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 10111
+            else if (species != 'all' && group.toUpperCase() == '' && protein.toUpperCase() != '' && localization.toUpperCase() != '' && bprocess.toUpperCase() != '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 11000
+            else if (species != 'all' && group.toUpperCase() != '' && protein.toUpperCase() == '' && localization.toUpperCase() == '' && bprocess.toUpperCase() == '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 11001
+            else if (species != 'all' && group.toUpperCase() != '' && protein.toUpperCase() == '' && localization.toUpperCase() == '' && bprocess.toUpperCase() != '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 11010
+            else if (species != 'all' && group.toUpperCase() != '' && protein.toUpperCase() == '' && localization.toUpperCase() != '' && bprocess.toUpperCase() == '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 11011
+            else if (species != 'all' && group.toUpperCase() != '' && protein.toUpperCase() == '' && localization.toUpperCase() != '' && bprocess.toUpperCase() != '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 11100
+            else if (species != 'all' && group.toUpperCase() != '' && protein.toUpperCase() != '' && localization.toUpperCase() == '' && bprocess.toUpperCase() == '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 11101
+            else if (species != 'all' && group.toUpperCase() != '' && protein.toUpperCase() != '' && localization.toUpperCase() == '' && bprocess.toUpperCase() != '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 11110
+            else if (species != 'all' && group.toUpperCase() != '' && protein.toUpperCase() != '' && localization.toUpperCase() != '' && bprocess.toUpperCase() == '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 11111
+            else if (species != 'all' && group.toUpperCase() != '' && protein.toUpperCase() != '' && localization.toUpperCase() != '' && bprocess.toUpperCase() != '') {
+              if (rowSpecies.toUpperCase().indexOf(species.toUpperCase()) != -1 && rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 01001
+            else if (species == 'all' && group.toUpperCase() != '' && protein.toUpperCase() == '' && localization.toUpperCase() == '' && bprocess.toUpperCase() != '') {
+              if (rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 01010
+            else if (species == 'all' && group.toUpperCase() != '' && protein.toUpperCase() == '' && localization.toUpperCase() != '' && bprocess.toUpperCase() == '') {
+              if (rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 01011
+            else if (species == 'all' && group.toUpperCase() != '' && protein.toUpperCase() == '' && localization.toUpperCase() != '' && bprocess.toUpperCase() != '') {
+              if (rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 01100
+            else if (species == 'all' && group.toUpperCase() != '' && protein.toUpperCase() != '' && localization.toUpperCase() == '' && bprocess.toUpperCase() == '') {
+              if (rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 01101
+            else if (species == 'all' && group.toUpperCase() != '' && protein.toUpperCase() != '' && localization.toUpperCase() == '' && bprocess.toUpperCase() != '') {
+              if (rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 01110
+            else if (species == 'all' && group.toUpperCase() != '' && protein.toUpperCase() != '' && localization.toUpperCase() != '' && bprocess.toUpperCase() == '') {
+              if (rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 01111
+            else if (species == 'all' && group.toUpperCase() != '' && protein.toUpperCase() != '' && localization.toUpperCase() != '' && bprocess.toUpperCase() != '') {
+              if (rowGroup.toUpperCase().indexOf(group.toUpperCase()) != -1 && rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 00101
+            else if (species == 'all' && group.toUpperCase() == '' && protein.toUpperCase() != '' && localization.toUpperCase() == '' && bprocess.toUpperCase() != '') {
+              if (rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 00110
+            else if (species == 'all' && group.toUpperCase() == '' && protein.toUpperCase() != '' && localization.toUpperCase() != '' && bprocess.toUpperCase() == '') {
+              if (rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 00111
+            else if (species == 'all' && group.toUpperCase() == '' && protein.toUpperCase() != '' && localization.toUpperCase() != '' && bprocess.toUpperCase() != '') {
+              if (rowProtein.toUpperCase().indexOf(protein.toUpperCase()) != -1 && rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+            // 00011
+            else if (species == 'all' && group.toUpperCase() == '' && protein.toUpperCase() == '' && localization.toUpperCase() != '' && bprocess.toUpperCase() != '') {
+              if (rowLocalization.toUpperCase().indexOf(localization.toUpperCase()) != -1 && rowBProcess.toUpperCase().indexOf(bprocess.toUpperCase()) != -1) {
+                  $(this).show();
+              } else {
+                  $(this).hide();
+              }
+            }
+        });
+    }
+}
